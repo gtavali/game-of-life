81f86c840d149fca1b05b444da14b73882fd09df
Gabor Tavali <gtavali@precognox.com>
2018-05-10 20:37:26 +0200

    Created the initial Spring Boot application.

---
+++ src/main/java/com/gtavali/gameoflife/GameOfLifeApplication.java
@@ -0,0 +1,16 @@
+package com.gtavali.gameoflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gabor Tavali
+ */
+@SpringBootApplication
+public class GameOfLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOfLifeApplication.class, args);
+    }
+
+}

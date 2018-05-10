d379bb6ad7921822666796ebba3745abdace1e54
Gabor Tavali <gtavali@precognox.com>
2018-05-10 20:46:29 +0200

    - Added the skeleton of the service and controller classes. - Defined application.yml.

---
+++ src/main/java/com/gtavali/gameoflife/controllers/GameOfLifeController.java
@@ -0,0 +1,12 @@
+package com.gtavali.gameoflife.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Gabor Tavali
+ */
+@RestController
+@RequestMapping("/backend")
+public class GameOfLifeController {
+}
---
+++ src/main/java/com/gtavali/gameoflife/services/GameOfLifeService.java
@@ -0,0 +1,12 @@
+package com.gtavali.gameoflife.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * The BE service of the application.
+ *
+ * @author Gabor Tavali
+ */
+@Service
+public class GameOfLifeService {
+}
